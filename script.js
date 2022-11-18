790f383b670fcb39a03e2786355bf71c6221d354
Donovan Dillon <DonBaiDill@Gmail.com>
2022-11-18 11:22:51 -0600

    Improved how buttons work

--- script.js
+++ script.js
@@ -3,43 +3,77 @@ const addButton2 = document.querySelector(".button-add-2");
 const addButton3 = document.querySelector(".button-add-3");
 
 addButton1.addEventListener("click", addToList1);
+document.querySelector(".text-1").addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      addToList1();
+    }
+});
 addButton2.addEventListener("click", addToList2);
+document.querySelector(".text-2").addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      addToList2();
+    }
+});
 addButton3.addEventListener("click", addToList3);
+document.querySelector(".text-3").addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      addToList3();
+    }
+});
 
+//I really tried to get this to work as a function with arguments so there could be only one function instead of three but there is some horrible arcane
+//secret involved in getting "click" to work with a function that has parameters FOR SOME REASON??????????? so... this is what we get.
 function addToList1(){
-    let newLi = addItem();
-    document.querySelector(".list-today").appendChild(newLi);
+    let textBox = document.querySelector(".text-1");
+    if (textBox.value === ""){
+        document.querySelector(".text-1").focus();
+    }
+    else{
+        var newLi = document.createElement('li');
+        newLi.appendChild(document.createTextNode(textBox.value));
+        document.querySelector(".list-today").appendChild(newLi);
+
+        textBox.value = "";
+    }
 }
 
 function addToList2(){
-    let newLi = addItem();
-    document.querySelector(".list-eventually").appendChild(newLi);
+    let textBox = document.querySelector(".text-2");
+    if (textBox.value === ""){
+        document.querySelector(".text-2").focus();
+    }
+    else{
+        var newLi = document.createElement('li');
+        newLi.appendChild(document.createTextNode(textBox.value));
+        document.querySelector(".list-eventually").appendChild(newLi);
+
+        textBox.value = "";
+    }
 }
 
 function addToList3(){
-    let newLi = addItem();
-    const l1 = document.querySelector(".list-constant-1");
-    const l2 = document.querySelector(".list-constant-2");
-    const l3 = document.querySelector(".list-constant-3");
-
-    if (l1.childElementCount === l3.childElementCount){
-        l1.appendChild(newLi);
-    }
-    else if (l1.childElementCount > l2.childElementCount){
-        l2.appendChild(newLi);
+    let textBox = document.querySelector(".text-3");
+    if (textBox.value === ""){
+        document.querySelector(".text-3").focus();
     }
     else{
-        l3.appendChild(newLi);
-    }
-}
-
-function addItem(){
-    const item = prompt("Add an issue.");
-
-    if (item !== ""){
         var newLi = document.createElement('li');
-        newLi.appendChild(document.createTextNode(item));
+        newLi.appendChild(document.createTextNode(textBox.value));
+        
+        const l1 = document.querySelector(".list-constant-1");
+        const l2 = document.querySelector(".list-constant-2");
+        const l3 = document.querySelector(".list-constant-3");
+    
+        if (l1.childElementCount === l3.childElementCount){
+            l1.appendChild(newLi);
+        }
+        else if (l1.childElementCount > l2.childElementCount){
+            l2.appendChild(newLi);
+        }
+        else{
+            l3.appendChild(newLi);
+        }
 
-        return newLi;
+        textBox.value = "";
     }
 }
