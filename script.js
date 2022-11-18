ace7db87b0abe0dd1aa16ec2df25c088cea67d93
Donovan Dillon <DonBaiDill@Gmail.com>
2022-11-18 13:48:49 -0600

    Buttons work with a standard function now

--- script.js
+++ script.js
@@ -2,78 +2,61 @@ const addButton1 = document.querySelector(".button-add-1");
 const addButton2 = document.querySelector(".button-add-2");
 const addButton3 = document.querySelector(".button-add-3");
 
-addButton1.addEventListener("click", addToList1);
+addButton1.addEventListener("click", function () {
+    addToList(".text-1", ".list-today");
+});
 document.querySelector(".text-1").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-      addToList1();
+        addToList(".text-1", ".list-today");
     }
 });
-addButton2.addEventListener("click", addToList2);
+
+addButton2.addEventListener("click", function () {
+    addToList(".text-2", ".list-eventually");
+});
 document.querySelector(".text-2").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-      addToList2();
+        addToList(".text-2", ".list-eventually");
     }
 });
-addButton3.addEventListener("click", addToList3);
+
+addButton3.addEventListener("click", function () {
+    let listType = chooseConstantList();
+    addToList(".text-3", listType);
+});
 document.querySelector(".text-3").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-      addToList3();
+        let listType = chooseConstantList();
+        addToList(".text-3", listType);
     }
 });
 
-//I really tried to get this to work as a function with arguments so there could be only one function instead of three but there is some horrible arcane
-//secret involved in getting "click" to work with a function that has parameters FOR SOME REASON??????????? so... this is what we get.
-function addToList1(){
-    let textBox = document.querySelector(".text-1");
+function addToList(textNumber, listType){
+    let textBox = document.querySelector(textNumber);
     if (textBox.value === ""){
-        document.querySelector(".text-1").focus();
+        textBox.focus();
     }
     else{
-        var newLi = document.createElement('li');
+        let newLi = document.createElement('li');
         newLi.appendChild(document.createTextNode(textBox.value));
-        document.querySelector(".list-today").appendChild(newLi);
+        document.querySelector(listType).appendChild(newLi);
 
         textBox.value = "";
     }
 }
 
-function addToList2(){
-    let textBox = document.querySelector(".text-2");
-    if (textBox.value === ""){
-        document.querySelector(".text-2").focus();
+function chooseConstantList(){
+    const l1 = document.querySelector(".list-constant-1");
+    const l2 = document.querySelector(".list-constant-2");
+    const l3 = document.querySelector(".list-constant-3");
+
+    if (l1.childElementCount === l3.childElementCount){
+        return ".list-constant-1";
+    }
+    else if (l1.childElementCount > l2.childElementCount){
+        return ".list-constant-2";
     }
     else{
-        var newLi = document.createElement('li');
-        newLi.appendChild(document.createTextNode(textBox.value));
-        document.querySelector(".list-eventually").appendChild(newLi);
-
-        textBox.value = "";
-    }
-}
-
-function addToList3(){
-    let textBox = document.querySelector(".text-3");
-    if (textBox.value === ""){
-        document.querySelector(".text-3").focus();
-    }
-    else{
-        var newLi = document.createElement('li');
-        newLi.appendChild(document.createTextNode(textBox.value));
-        
-        const l1 = document.querySelector(".list-constant-1");
-        const l2 = document.querySelector(".list-constant-2");
-        const l3 = document.querySelector(".list-constant-3");
-    
-        if (l1.childElementCount === l3.childElementCount){
-            l1.appendChild(newLi);
-        }
-        else if (l1.childElementCount > l2.childElementCount){
-            l2.appendChild(newLi);
-        }
-        else{
-            l3.appendChild(newLi);
-        }
-
-        textBox.value = "";
+        return ".list-constant-3";
     }
 }
