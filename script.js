48dabcdd01487c52230dfe957cb36a57f91f0cd1
Donovan Dillon <DonBaiDill@Gmail.com>
2022-11-19 13:17:45 -0600

    Naming changes, first pass

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const addButton1 = document.querySelector(".button-add-1");
-const addButton2 = document.querySelector(".button-add-2");
-const addButton3 = document.querySelector(".button-add-3");
+const dailyButton = document.querySelector(".daily__button");
+const generalButton = document.querySelector(".general__button");
+const constantButton = document.querySelector(".constant__button");
 
-addButton1.addEventListener("click", function () {
-    addToList(".text-1", ".list-today");
+dailyButton.addEventListener("click", function () {
+    addToList(".daily__textBox", ".daily__list");
 });
-document.querySelector(".text-1").addEventListener("keydown", function (e) {
+document.querySelector(".daily__textBox").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        addToList(".text-1", ".list-today");
+        addToList(".daily__textBox", ".daily__list");
     }
 });
 
-addButton2.addEventListener("click", function () {
-    addToList(".text-2", ".list-eventually");
+generalButton.addEventListener("click", function () {
+    addToList(".general__textBox", ".general__list");
 });
-document.querySelector(".text-2").addEventListener("keydown", function (e) {
+document.querySelector(".general__textBox").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        addToList(".text-2", ".list-eventually");
+        addToList(".general__textBox", ".general__list");
     }
 });
 
-addButton3.addEventListener("click", function () {
+constantButton.addEventListener("click", function () {
     let listType = chooseConstantList();
-    addToList(".text-3", listType);
+    addToList(".constant__textBox", listType);
 });
-document.querySelector(".text-3").addEventListener("keydown", function (e) {
+document.querySelector(".constant__textBox").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
         let listType = chooseConstantList();
-        addToList(".text-3", listType);
+        addToList(".constant__textBox", listType);
     }
 });
 
@@ -46,17 +46,17 @@ function addToList(textNumber, listType){
 }
 
 function chooseConstantList(){
-    const l1 = document.querySelector(".list-constant-1");
-    const l2 = document.querySelector(".list-constant-2");
-    const l3 = document.querySelector(".list-constant-3");
+    const l1 = document.querySelector(".constant__list1");
+    const l2 = document.querySelector(".constant__list2");
+    const l3 = document.querySelector(".constant__list3");
 
     if (l1.childElementCount === l3.childElementCount){
-        return ".list-constant-1";
+        return ".constant__list1";
     }
     else if (l1.childElementCount > l2.childElementCount){
-        return ".list-constant-2";
+        return ".constant__list2";
     }
     else{
-        return ".list-constant-3";
+        return ".constant__list3";
     }
 }
